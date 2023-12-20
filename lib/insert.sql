1dc1cd9d093998fc8deb631f67a84a975224b4ad
Morgan VanYperen <morgvanny@gmail.com>
2023-12-20 10:51:26 -0800

    done

---
+++ lib/insert.sql
@@ -0,0 +1,47 @@
+INSERT INTO bears (
+  id, name, age, sex, color, temperament, alive
+) VALUES (
+  1, "Mr. Chocolate", 100, "M", "Brown", "Jolly", 1
+);
+
+INSERT INTO bears (
+  id, name, age, sex, color, temperament, alive
+) VALUES (
+  2, "Rowdy", 100, "M", "Brown", "Jolly", 1
+);
+
+INSERT INTO bears (
+  id, name, age, sex, color, temperament, alive
+) VALUES (
+  3, "Tabitha", 100, "M", "Brown", "Jolly", 1
+);
+
+INSERT INTO bears (
+  id, name, age, sex, color, temperament, alive
+) VALUES (
+  4, "Sergeant Brown", 100, "M", "Brown", "Jolly", 1
+);
+
+INSERT INTO bears (
+  id, name, age, sex, color, temperament, alive
+) VALUES (
+  5, "Melissa", 100, "M", "Brown", "Jolly", 1
+);
+
+INSERT INTO bears (
+  id, name, age, sex, color, temperament, alive
+) VALUES (
+  6, "Grinch", 100, "M", "Brown", "Jolly", 1
+);
+
+INSERT INTO bears (
+  id, name, age, sex, color, temperament, alive
+) VALUES (
+  7, "Wendy", 100, "M", "Brown", "Jolly", 1
+);
+
+INSERT INTO bears (
+  id, age, sex, color, temperament, alive
+) VALUES (
+  8, 100, "M", "Brown", "Jolly", 1
+);
